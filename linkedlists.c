6d9d6a0147eb62f11ab5bdb3a7801632bc459681
Erfan <lapechie@Erfans-Macbook-Pro-M1.local>
2022-06-15 08:10:12 +1000

    created functions

--- linkedlists.c
+++ linkedlists.c
@@ -5,21 +5,92 @@
 // doubly
 // circular
 // doubly circular
+struct node
+{   
+    int data;
+    struct node *next;
+};
+
+void display_ll(struct node* head)
+{
+    printf("--------------------------------\n");
+    struct node *temp;
+    temp = head;
+    while(temp != 0)
+    {
+        printf("\n%d\n", temp->data);
+        temp = temp->next;
+    }
+}
+
+void delete_from_beg(struct node* head)
+{
+    struct node *temp;
+    temp = head;
+    head = head->next; 
+    free(temp); 
+    display_ll(head);
+}
+
+void insert_beg(struct node* head)
+{
+    struct node* newnode;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    printf("Enter data you want to insert:\n");
+    scanf("%d", &newnode->data);
+    newnode->next = head;
+    head = newnode;
+    display_ll(head);
+}
+
+void insert_end(struct node* head)
+{
+    struct node* newnode;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    struct node* temp;
+    printf("Enter data you want to insert:\n");
+    scanf("%d", &newnode->data);
+    newnode->next = 0;
+    temp = head;
+
+    while(temp->next != 0)
+    {
+        temp = temp->next;
+    }
+    temp->next = newnode;
+    display_ll(head);
+}
+
+void insert_at_loc(struct node* head)
+{
+    struct node* newnode;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    struct node* temp;
+    int pos;
+    int i = 1;
+    printf("Enter position to insert:\n");
+    scanf("%d", &pos);
+    temp = head;
+    while(i < pos)
+    {
+        temp = temp->next;
+        i++;
+    }
+    printf("Enter data you want to insert:\n");
+    scanf("%d", &newnode->data);
+    newnode->next = temp->next;
+    temp->next = newnode;
+    display_ll(head);
+}
 
 void main()
 {
-    struct node
-    {   
-        int data;
-        struct node *next;
-    };
-
     struct node *head;
     struct node *newnode;
     struct node *temp;
     head = NULL;
     int choice;
-    
+    // creating a linked list
     while(choice)
     {   
         newnode = (struct node*)malloc(sizeof(struct node));
@@ -40,12 +111,15 @@ void main()
         printf("Do you want to coninue? (0,1) \n");
         scanf("%d", &choice);
     }
-    printf("--------------------------------\n");
-    temp = head;
-    while(temp != 0)
-    {
-        printf("\n%d\n", temp->data);
-        temp = temp->next;
-    }
-    
+
+    display_ll(head);
+
+    //insert_beg(head);
+
+    //insert_end(head);
+
+    //insert_at_loc(head);
+
+    delete_from_beg(head);
+
 }
